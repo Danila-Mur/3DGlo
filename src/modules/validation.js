9b67a1f1bdfda07fa693852922e6874a4bd5a577
Danil Murzin <danil.murzin.200026@gmail.com>
2022-08-31 13:38:20 +0300

    fix validation

--- src/modules/validation.js
+++ src/modules/validation.js
@@ -1,29 +1,32 @@
 const validation = () => {
   const calcItem = document.querySelectorAll('.calc-item'),
-    topForm = document.querySelector('#form1'),
-    nameTopInput = topForm.querySelector('input[type=text]'),
-    emailTopInput = topForm.querySelector('input[type=email]'),
-    telTopInput = topForm.querySelector('input[type=tel]'),
-    bottomForm = document.querySelector('#form2'),
-    nameBottomInput = bottomForm.querySelector('input[type=text]'),
-    messageBottom = bottomForm.querySelector('.mess'),
-    emailBottomInput = bottomForm.querySelector('input[type=email]'),
-    telBottomInput = bottomForm.querySelector('input[type=tel]'),
-    popupForm = document.querySelector('#form3'),
-    namePopupInput = popupForm.querySelector('input[type=text]'),
-    emailPopupInput = popupForm.querySelector('input[type=email]'),
-    telPopupInput = popupForm.querySelector('input[type=tel]');
+    textInputs = [
+      document.querySelector('#form1 input[type=text]'),
+      document.querySelector('#form2 input[type=text]'),
+      document.querySelector('#form2 .mess'),
+      document.querySelector('#form3 input[type=text]'),
+    ],
+    emailInputs = [
+      document.querySelector('#form1 input[type=email]'),
+      document.querySelector('#form2 input[type=email]'),
+      document.querySelector('#form3 input[type=email]'),
+    ],
+    telInputs = [
+      document.querySelector('#form1 input[type=tel]'),
+      document.querySelector('#form2 input[type=tel]'),
+      document.querySelector('#form3 input[type=tel]'),
+    ];
 
-  const validationCyrillic = (input) => {
-    return !/[^а-яА-я\-\s]/.test(input.value) && input.value.trim() !== '';
+  const validationCyrillic = (e) => {
+    e.target.value = e.target.value.replace(/[^а-яА-я\-\s]/, '');
   };
 
-  const validationEmail = (email) => {
-    return /\S+[\w\.-]+@\S+\.\S+/.test(email.value) && email.value.trim() != '';
+  const validationEmail = (e) => {
+    e.target.value = e.target.value.replace(/\S+[\w\.-]+@\S+\.\S+/, '');
   };
 
-  const validationTel = (tel) => {
-    return /^[0-9()\-]+$/.test(tel.value) && tel.value.trim() != '';
+  const validationTel = (e) => {
+    e.target.value = e.target.value.replace(/[a-zА-я!@#$%&*_+=]+$/, '');
   };
 
   calcItem.forEach((item) => {
@@ -32,91 +35,16 @@ const validation = () => {
     });
   });
 
-  topForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let isError = false;
-
-    if (validationCyrillic(nameTopInput)) {
-      alert('Кирилица topForm');
-    } else {
-      isError = true;
-    }
-
-    if (validationEmail(emailTopInput)) {
-      alert('email');
-    } else {
-      isError = true;
-    }
-
-    if (validationTel(telTopInput)) {
-      alert('tel');
-    } else {
-      isError = true;
-    }
-
-    if (!isError) {
-      alert('Done');
-    }
+  textInputs.forEach((input) => {
+    input.addEventListener('input', validationCyrillic);
   });
 
-  bottomForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let isError = false;
-
-    if (validationCyrillic(nameBottomInput)) {
-      alert('Кирилица bottomForm input');
-    } else {
-      isError = true;
-    }
-
-    if (validationCyrillic(messageBottom)) {
-      alert('Кирилица bottomForm textarea');
-    } else {
-      isError = true;
-    }
-
-    if (validationEmail(emailBottomInput)) {
-      alert('email');
-    } else {
-      isError = true;
-    }
-
-    if (validationTel(telBottomInput)) {
-      alert('tel');
-    } else {
-      isError = true;
-    }
-
-    if (!isError) {
-      alert('Done');
-    }
+  emailInputs.forEach((email) => {
+    email.addEventListener('input', validationEmail);
   });
 
-  popupForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let isError = false;
-
-    if (validationCyrillic(namePopupInput)) {
-      alert('Кирилица bottomForm input');
-    } else {
-      isError = true;
-    }
-
-    if (validationEmail(emailPopupInput)) {
-      alert('email popup');
-    } else {
-      isError = true;
-    }
-
-    if (validationTel(telPopupInput)) {
-      alert('tel');
-    } else {
-      isError = true;
-    }
-
-    if (!isError) {
-      alert('Done');
-    }
+  telInputs.forEach((tel) => {
+    tel.addEventListener('input', validationTel);
   });
 };
 
