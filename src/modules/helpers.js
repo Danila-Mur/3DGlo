664ca7216c20a571703f2f1f44efabaa8426a756
Danil Murzin <danil.murzin.200026@gmail.com>
2022-09-08 18:13:28 +0300

    add homework for lesson27

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -17,4 +17,57 @@ const animate = ({ timing, draw, duration }) => {
   });
 };
 
-export { animate };
+const validate = (list) => {
+  let success = true;
+
+  const validationName = (input) => {
+    if (input.value.length >= 2 && input.value.trim()) {
+      return true;
+    }
+  };
+
+  const validationPhone = (input) => {
+    if (
+      input.value.replace(
+        /^(\+)?((\d{2,3}) ?\d|\d)(([ -]?\d)|( ?(\d{2,3}) ?)){5,12}\d$/,
+        ''
+      )
+    ) {
+      return true;
+    }
+  };
+
+  const validationEmail = (input) => {
+    if (
+      input.value.replace(
+        /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/,
+        ''
+      )
+    ) {
+      return true;
+    }
+  };
+
+  list.forEach((input) => {
+    // if (!input.classList.contains('success')) {
+    //   success = false;
+    // }
+    if (input.getAttribute('name') === 'user_name') {
+      if (!validationName(input)) {
+        success = false;
+      }
+    } else if (input.getAttribute('name') === 'user_phone') {
+      if (!validationPhone(input)) {
+        success = false;
+      }
+    } else if (input.getAttribute('name') === 'user_email') {
+      if (!validationEmail(input)) {
+        success = false;
+      }
+    }
+  });
+
+  return success;
+};
+
+export { animate, validate };
