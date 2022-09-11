148b30382f80c9e40c13e1ad171d302eff8e26fd
Danil Murzin <danil.murzin.200026@gmail.com>
2022-09-11 21:24:22 +0300

    fix total

--- src/index.js
+++ src/index.js
@@ -19,5 +19,5 @@ tabs();
 calc(100);
 maskPhone('*[name="user_phone"]', '+7 (___)-___-__-__');
 sendForm({ formId: '#form1', someElem: [{ type: 'block', id: 'total' }] });
-sendForm({ formId: '#form2' });
-sendForm({ formId: '#form3' });
+sendForm({ formId: '#form2', someElem: [{ type: 'block', id: 'total' }]  });
+sendForm({ formId: '#form3', someElem: [{ type: 'block', id: 'total' }] });
